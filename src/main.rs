400ebb89be8dbea5b1127c416e01125e1f5479c6
Torben Carstens <git@carstens.tech>
2021-11-16 16:29:51 +0100

    Do you wanna build a Result?

--- src/main.rs
+++ src/main.rs
@@ -124,7 +124,7 @@ async fn answer(
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     // TODO: This should be assembled from members of the group at some point (talk to API for that)
     if cx.update.chat.id != env::var("ADMIN_CHAT")?.parse::<i64>()? {
-        return cx.answer("You're not allowed to use this bot.").await?;
+        return Ok(cx.answer("You're not allowed to use this bot.").await?);
     }
 
     match command {
